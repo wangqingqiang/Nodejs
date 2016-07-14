fd703d991c1426d8c107954958f734f58c8085b7
wangqingqiang <qingqiang.wang@qq.com>
2016-07-15 02:44:17 +0800

    ..
    ..

--- Controllers/albumRouter.js
+++ Controllers/albumRouter.js
@@ -1,14 +1,79 @@
 /**
  * Created by wqq on 2016/7/14.
  */
-function showIndex(req,res){
-    res.render('albumIndex');
+var albumModel = require('../Models/albumModel.js');
+
+function showIndex(req, res) {
+    //res.render('albumIndex');
+    //albumModel.getAlbums(function(error,albums){
+    //    if(error){
+    //        console.log(error);
+    //        res.status(404);
+    //        res.render('404');
+    //    }else {
+    //        console.log(albums);
+    //        res.send(albums.toString());
+    //
+    //    }
+    //})
+    var albums = albumModel.getAlbumsSync();
+    if (albums.length) {
+        res.render('albumIndex', {albums: albums});
+    } else {
+        res.status(404);
+        res.render('404');
+    }
 }
-function show404(req,res){
+function showAlbum(req, res) {
+    var album = req.params.album;
+    albumModel.getPhotos(album, function (error, photos) {
+        if (error) {
+            console.log(error);
+        }
+        else {
+            res.render('albumPhoto', {album: album, photos: photos});
+        }
+    })
+}
+function showPhoto(req,res){
+    var pathname='/upload/album/'+req.params.albumName+'/'+req.params.id;
+    console.log(pathname);
+    //albumModel.getPhoto(pathname,function(error,data){
+    //    if(error){
+    //        console.log(error);
+    //    }else {
+    //
+    //    }
+    //})
+    var options = {
+        root: './',
+        dotfiles: 'deny',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    };
+
+    var fileName = pathname;
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+        else {
+            console.log('Sent:', fileName);
+        }
+    });
+
+}
+function show404(req, res) {
+    res.status(404);
     res.render('404')
 }
 
-module.exports={
+module.exports = {
     showIndex,
-    show404
+    show404,
+    showAlbum,
+    showPhoto
 }
--- app.js
+++ app.js
@@ -10,9 +10,14 @@ app.set('view engine','ejs');
 //注意此处是 /static路由 ，但是目录是 ./Public，一个带 '.'，一个不带
 app.use('/static',express.static('./Public'));
 
-app.use('/',albumRouter.showIndex)
-
+// 路由前面都是没有'.'的，否则匹配不上，
+//注意此处不是app.use，app.use不是精确匹配，它可以匹配到书写的路由后面的子路由，
+// 例如app.use(/admin,callback)可以匹配到 /admin/adminlist/adminid等
+app.get('/',albumRouter.showIndex);
+//设置相册详情页路由
+app.get('/favicon.ico',function(req,res){});
+app.get('/:album',albumRouter.showAlbum)
+app.get('/upload/album/:albumName/:id',albumRouter.showPhoto);
 app.use(albumRouter.show404);
 app.listen(8080,(err)=>{
-    console.log(err);
 })
