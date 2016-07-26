83ad887ccbceaad15f81049f98248af3eeecfd48
wangqingqiang <qingqiang.wang@qq.com>
2016-07-26 09:09:38 +0800

    Revert "删除GraphicsMagick"
    This reverts commit 0f754c8543ee1d3491a52d2cde8b6a76acd0d5cf.

--- 02.js
+++ 02.js
@@ -1,10 +1,18 @@
 /**
  * Created by wqq on 2016/7/1.
  */
+const net = require('net');
+var server = net.createServer(serverConnection);
+server.listen(8080, (error)=> {
+    if (error) {
+        console.log('端口8080已经被占');
+    }
+    else {
+        console.log('端口8080分配成功');
+    }
+})
+function serverConnection(socket) {
+    console.log('someone is connecting');
+    console.log(socket.address().address);
 
-function helloWorld() {
-    console.log('hello world！');
 }
-module.exports={hello:helloWorld};
-
-module.exports={helloWorld};
